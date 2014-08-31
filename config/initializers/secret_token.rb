e1e090b165b2138fe9ac93c1261885f2ca53cd6a
Michael Scherbaum <mscherbaum@salesforce.com>
2014-08-31 16:36:59 +0100

    re-initialized repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsStripeMembershipSaas::Application.config.secret_token = 'ffe08da322362fe593136dd3dbc5159066aa1c443de018aed8a1f628610fbbea8b8083efb7db413b8e8ce018a26f266f1f43925f851ec9362e828581576531f8'
